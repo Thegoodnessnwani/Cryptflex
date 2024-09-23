d89a1618a25080d9d34dffe0ac4fe47789a9ef70
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2024-09-23 11:13:09 +0100

    feat: Setup typography - desktop fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-    return <main>Main App</main>;
+    return (
+        <main>
+            <p className="font-bold text-2xl text-center">Hello I am at work</p>
+        </main>
+    );
 }
